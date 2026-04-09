cbabbb79f7e4ada8dca9aa23c092b6ccbfff18ad
Clodoaldo Brasilino <886522+clodbrasilino@users.noreply.github.com>
2026-04-09 17:30:53 +0800

    fix: use convert_tokens_to_ids instead of deprecated additional_special_tokens_ids (transformers 5.x)

--- src/BLIP_finetune/models/blip.py
+++ src/BLIP_finetune/models/blip.py
@@ -186,8 +186,8 @@ def blip_feature_extractor(pretrained='',**kwargs):
 def init_tokenizer():
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
     tokenizer.add_special_tokens({'bos_token':'[DEC]'})
-    tokenizer.add_special_tokens({'additional_special_tokens':['[ENC]']})       
-    tokenizer.enc_token_id = tokenizer.additional_special_tokens_ids[0]  
+    tokenizer.add_special_tokens({'additional_special_tokens': ['[ENC]']})
+    tokenizer.enc_token_id = tokenizer.convert_tokens_to_ids('[ENC]')  
     return tokenizer
 
 
